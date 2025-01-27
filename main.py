6991731ad178a8b4503bcea1c08345ab157ea60b
Avish Madhow <avish.madhow@gmail.com>
2025-01-27 15:02:00 +0000

    Fixed env path problem

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 import os
 
 # link morse code dictionary API
-# load_dotenv(r"C:\Users\vikas\Google Drive\Avish\Udemy\PythonBootcamp\Projects\MorseCode\.env")
-# morse_code_dict_api = os.getenv('API_Endpoint')
-morse_code_dict_api = 'https://api.npoint.io/11d0b507c4a9a403ca01'
+load_dotenv(r"C:\Users\vikas\Google Drive\Avish\Udemy\PythonBootcamp\Projects\MorseCode\MorseCode\.env")
+
+morse_code_dict_api = os.getenv('API_Endpoint')
 route = requests.get(url=morse_code_dict_api)
 response = route.json()
 
