67733c963bdf04772371cd5fc27d49ce85c6f5d8
Avish Madhow <avish.madhow@gmail.com>
2025-01-27 11:29:10 +0000

    Tried to encrypt api endpoint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 # link morse code dictionary API
+# load_dotenv(r"C:\Users\vikas\Google Drive\Avish\Udemy\PythonBootcamp\Projects\MorseCode\.env")
+# morse_code_dict_api = os.getenv('API_Endpoint')
 morse_code_dict_api = 'https://api.npoint.io/11d0b507c4a9a403ca01'
 route = requests.get(url=morse_code_dict_api)
 response = route.json()
