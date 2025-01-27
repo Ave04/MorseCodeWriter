385a5dfc141b7e2675a613c7e0c65e5b7e47e95d
Avish Madhow <avish.madhow@gmail.com>
2025-01-27 17:03:33 +0000

    generalised path for load_dotenv

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 # link morse code dictionary API
-load_dotenv(r"C:\Users\vikas\Google Drive\Avish\Udemy\PythonBootcamp\Projects\MorseCode\MorseCode\.env")
+load_dotenv(r"Enter Path for .env file here")
 
 morse_code_dict_api = os.getenv('API_Endpoint')
 route = requests.get(url=morse_code_dict_api)
